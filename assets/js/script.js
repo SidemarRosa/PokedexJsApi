1659ba89bf6a8152d5a0c434e18b29a4eaccd348
SidemarRosa <sidr.com.slr@gmail.com>
2024-02-22 16:00:19 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 //variaveis e constantes
-const offset = 0
+const offset = 10
 const limmit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&immit=${limmit}`
 
 // Função fecth para requisição da api
 fetch(url)
     //função de callback
-    .then((response) => return response.json())   // transforma as strings do body da resposta da api em json com promisse de any
+    .then((response) =>  response.json())   // transforma as strings do body da resposta da api em json com promisse de any
     .then((jsonBody) => console.log(jsonBody)) //modifica o nome para jsonbody
     .catch ((error) => console.error(error)) //tratamento de erro
-    //finally quando preciso usar ------        .finally( () => console.log('requisição concluida')) //mensagem de finalização
+    //finally quando preciso usar ------        .finally( () => console.log('requisição concluida')) //mensagem de finalização independente do sucesso ou do fracasso
    
     
